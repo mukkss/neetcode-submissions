2fb7e151fb57257c6b15eb349f81ce9cef610cda
Mukesh <149927808+mukkss@users.noreply.github.com>
2026-08-03 11:37:03 +0530

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string encoded = "";
+
+        for(string s : strs){
+            encoded += to_string(s.length()) + "#" + s;
+        }
+        return encoded;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> result;
+        int i = 0;
+        while(i < s.length()){
+            int j = i;
+            while(s[j] != '#'){
+                j++;
+            }
+
+            int len = stoi(s.substr(i, j - i));
+            j++;
+
+            result.push_back(s.substr(j, len));
+            i = j + len;
+        }
+        return result;
+    }
+};
