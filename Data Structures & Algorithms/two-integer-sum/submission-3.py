5c66bd4be2c0934a9a31e528d895d15d0fb3eae6
Mukesh <149927808+mukkss@users.noreply.github.com>
2026-08-08 16:22:45 +0530

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.py
@@ -0,0 +1,10 @@
+from collections import defaultdict
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        prevMap = defaultdict()
+
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
